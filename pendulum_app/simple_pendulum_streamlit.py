5f7be16a933c15adc4582e652219d8d974295674
anvify <anvify2022@gmail.com>
2025-05-23 21:20:01 +0530

    Update simple_pendulum_streamlit.py

--- pendulum_app/simple_pendulum_streamlit.py
+++ pendulum_app/simple_pendulum_streamlit.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 
 # Streamlit UI
 st.title("Simple Pendulum Simulation 🌗")
@@ -11,38 +10,26 @@ mass = st.slider("Mass (kg)", 0.1, 10.0, 1.0, 0.1)
 theta0_deg = st.slider("Initial Angle (degrees)", -90, 90, 30)
 g = st.slider("Gravity (m/s²)", 1.0, 20.0, 9.8, 0.1)
 
-theta0 = np.radians(theta0_deg)
+# Time control
+t_max = 10
+t_slider = st.slider("Time (s)", 0.0, float(t_max), 0.0, step=0.05)
 
-# Solve pendulum equation using small angle approximation
-t = np.linspace(0, 10, 1000)
+theta0 = np.radians(theta0_deg)
 omega = np.sqrt(g / length)
-theta = theta0 * np.cos(omega * t)
+theta = theta0 * np.cos(omega * t_slider)
 
 x = length * np.sin(theta)
 y = -length * np.cos(theta)
 
-# Plot the pendulum motion
+# Plot
 fig, ax = plt.subplots()
 ax.set_aspect('equal')
 ax.set_xlim(-length - 0.2, length + 0.2)
 ax.set_ylim(-length - 0.2, 0.2)
-ax.set_title("Pendulum Motion")
+ax.set_title("Pendulum at t = {:.2f}s".format(t_slider))
 ax.set_xlabel("x (m)")
 ax.set_ylabel("y (m)")
-
-line, = ax.plot([], [], 'o-', lw=2, color='darkblue')
-trail, = ax.plot([], [], 'r--', lw=0.5)
-
-def init():
-    line.set_data([], [])
-    trail.set_data([], [])
-    return line, trail
-
-def update(frame):
-    line.set_data([0, x[frame]], [0, y[frame]])
-    trail.set_data(x[:frame], y[:frame])
-    return line, trail
-
-ani = FuncAnimation(fig, update, frames=len(t), init_func=init, blit=True)
+ax.plot([0, x], [0, y], 'o-', lw=2, color='darkblue')
+ax.plot(x, y, 'ro')  # bob
 
 st.pyplot(fig)
